9e422597a40c36158dedf2327194f1c04240905d
macexcel <mexcelsf@yahoo.com>
2015-12-08 00:43:15 +0800

    commit

---
+++ app/src/main/java/macexcel/example/com/greenwall/Sound.java
@@ -0,0 +1,12 @@
+package macexcel.example.com.greenwall;
+
+/**
+ * Created by macexcel on 12/8/2015.
+ */
+
+/**
+ * Created by ugliest on 2/28/15.
+ */
+public enum Sound {
+    COMBO, THROW, SPLAT, WETSPLAT, KSPLAT, PASSLEVEL, DEATH
+}
